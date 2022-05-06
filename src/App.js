9ec6e746cf78bc7681185cdea3606c17c444cd14
ignacio cardozo <ignaciocardozomorales@gmail.com>
2022-05-06 10:18:06 -0400

    primer-componente

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="card">
+        <div class="card_header red">
+            <div class="tittle">
+                <div class="circle">1</div>
+                <p>Agenda/Topic</p>
+            </div>
+            <div class="id">
+                <p>SAAS-000</p>
+            </div>
+        </div>
+        <div class="card_section">
+            <p>Descripcion de la card</p>
+        </div>
+        <div class="card_footer">
+            <p>Mis Actualizaciones</p>
+            <p>14/07/2021</p>
+        </div>
+    </div>
     </div>
   );
 }
